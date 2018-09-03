5cdebd2a7dce901e3fa0c026a73c6757a0e6f17e
Th3_M4j0r <spartan364@gmail.com>
2018-09-03 08:17:00 -0700

    Added quiet version of dmCheck

--- functions/dmCheck.js
+++ functions/dmCheck.js
@@ -17,9 +17,11 @@ const disabledDMs = require(`../functions/disabledDMs.js`);
 
 /**
  * 
+ * Checks if they are in a DM channel
+ * If it was a DM, tells the user it was an invalid channel
  * @param {Discord.Message} message
  * @param {string} name
- * @returns {boolean}
+ * @returns {boolean} returns true if the message was in a DM channel
  */
 module.exports.run = (message, name) => {
     if (message.channel.type === "dm") { // If Sent in DM...
@@ -32,4 +34,14 @@ module.exports.run = (message, name) => {
         return true;
     }
     return false;
+}
+
+/**
+ * Checks if they are in a DM channel
+ * does not send the user anything
+ * @param {Discord.Message} message 
+ * @returns {boolean} is the message a DM?
+ */
+module.exports.runQuiet = (message) => {
+    return message.channel.type === "dm"
 }

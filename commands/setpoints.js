d7bb0f28c9c08e560f670327ebc5c6d54b417896
Th3_M4j0r <spartan364@gmail.com>
2018-09-16 12:55:33 -0700

    added setpoints and updated comments

---
+++ commands/setpoints.js
@@ -0,0 +1,94 @@
+/*
+    Command Name: setpoints.js
+    Function: Allows for manual setting of points for testing purposes.
+    Clearance: Owner Only
+	Default Enabled: no
+    Date Created: 11/03/17
+    Last Updated: 03/27/18
+    Last Updated By: Th3_M4j0r
+*/
+
+// Load in Required Files
+const config = require(`../files/config.json`);
+const enabled = require(`../files/enabled`);
+const debug = require(`../functions/debug.js`);
+const disabledCommand = require(`../functions/disabledCommand.js`);
+const Discord = require(`discord.js`);
+const dmCheck = require(`../functions/dmCheck.js`);
+const userids = require(`../files/userids.json`)
+const betterSql = require(`../functions/betterSql.js`);
+
+// Command Stuff
+
+const command = {
+    bigDescription: ("Use this command to set the points of a user to something else"),
+    description: "Changes a mentioned user's points",
+    enabled: enabled.setPoints,
+    name: "Set Points",
+    permissionLevel: "owner"
+}
+
+
+/**
+ * 
+ * @param {Discord.Client} bot
+ * @param {Discord.Message} message
+ * @param {string[]} args
+ * @param {betterSql} sql
+ */
+module.exports.run = async (bot, message, args, sql) => {
+    // Debug to Console Log
+    debug.log(`I am inside the ${command.name} Command.`);
+    if (dmCheck.run(message, command.name)) return;
+
+
+    // Owner ID Check
+    if (message.author.id !== userids.ownerID) { // If not Owner ID
+        return debug.log(`Attempted use of ${command.name} by ${message.author.username}.\n`);
+    }
+
+    // Enabled Command Test
+    if (!command.enabled) {
+        disabledCommand.run(command.name, message);
+    }
+
+    // Get the name of the Member to Change Points
+    var toChange = message.mentions.members.first();
+    // Get the amount of points to set
+    var amount = !!parseInt(message.content.split(" ")[1]) ? parseInt(message.content.split(" ")[1]) : parseInt(message.content.split(" ")[2]);
+
+    // Validation Check
+    if (!toChange) {
+        message.channel.send("You must mention someone to update their points");
+        return debug.log("No member was given.\n");
+    }
+    if (!amount) {
+        message.channel.send("You must indicate what to set their points to");
+        return debug.log("No new value was given.\n");
+    }
+
+    let row = await sql.getUserRow(toChange.id);
+    if (!row) {
+        message.channel.send(`I'm sorry, ${message.author}, I am unable to set the points of ${toChange.user.username} `
+            + `as they are not currently in the user database.`);
+        return debug.log(`Unable to locate any data for ${toChange.user.username}`);
+    }
+    if (row.optOut === 1) {
+        message.channel.send(`I'm sorry, ${message.author}, I cannot set the points of ${toChange.user.username} who has opted out`);
+        return debug.log(`Unable to set points of  who has opted out`);
+    }
+    let name = toChange.user.username;
+    try {
+        name = message.guild.members.get(toChange.id).nickname;
+        if (!name) name = toChange.user.username;
+        debug.log(`Name set to: ${name}`);
+    }
+    catch (error) {
+        name = message.author.username;
+        debug.log(`Unable to get Nickname. Name set to: ${name}`);
+    }
+    debug.log(`Setting points for ${name} to ${amount} from ${row.points}`);
+    sql.setPoints(toChange.id, amount, row.level, name);
+};
+
+module.exports.help = command;

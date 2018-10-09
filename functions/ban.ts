35887faba9104a0c944041b0c962bcc6df98bf6f
Th3_M4j0r <spartan364@gmail.com>
2018-10-09 08:30:42 -0700

    converted ban to TypeScript

--- functions/ban.ts
+++ functions/ban.ts
@@ -4,16 +4,17 @@
     Version: 3
     Author: AllusiveBox
     Date Started: 02/28/18
-    Date Last Updated: 10/07/18
-    Last Update By: AllusiveBox
+    Date Last Updated: 10/09/18
+    Last Update By: Th3_M4j0r
 
 **/
 
 // Load in Required Libraries and Files
-const Discord = require(`discord.js`);
-const config = require(`../files/config.json`);
-const channels = require(`../files/channels.json`);
-const { debug, error: errorLog } = require(`../functions/log.js`);
+import * as Discord from 'discord.js';
+import { logChannelColors } from '../files/config.json';
+import { ownerID } from '../files/userids.json';
+import { log } from '../files/channels.json';
+import { debug, error as errorLog } from './log.js';
 
 
 /**
@@ -23,21 +24,21 @@ const { debug, error: errorLog } = require(`../functions/log.js`);
  * @param {Discord.GuildMember} member
  * @param {string} reason
  */
-module.exports.run = async (bot, message, member, reason) => {
+export async function run(bot: Discord.Client, message: Discord.Message, member: Discord.GuildMember, reason: string) {
     // Debug to Console
     debug(`I am inside the ban function.`);
 
-    let logchannelColor = config.logChannelColors.memberBan;
+    let logchannelColor = logChannelColors.memberBan;
 
     // Load in the Log Channel ID
-    let logID = channels.log;
+    let logID = log;
     // Check if there was an ID Provided
     if (!logID) { // If no Log ID...
         debug(`Unable to find the log ID in channels.json.`
             + `Looking for another log channel.`);
 
         // Look for Log Channel in Server
-        logChannel = message.member.guild.channels.find(val => val.name === "log");
+        let logChannel = message.member.guild.channels.find(val => val.name === "log");
         if (!logChannel) { // If Unable to Find Log Channel...
             debug(`Unable to find any kind of log channel.`);
         } else {
@@ -69,9 +70,10 @@ module.exports.run = async (bot, message, member, reason) => {
 
     // Check if there is an ID Now
     if (!logID) { // If no Log ID...
-        bot.users.get(userids.ownerID).send(bannedEmbed);
+        bot.users.get(ownerID).send(bannedEmbed);
     } else {
-        bot.channels.get(logID).send(bannedEmbed);
+        let Channel = <Discord.TextChannel>bot.channels.get(logID);
+        Channel.send(bannedEmbed);
     }
 
     return debug(`Ban Successful.`);

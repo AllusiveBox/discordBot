254d1f04f11efc416db48a9ae29b26da1581007e
AllusiveBox <42219971+AllusiveBox@users.noreply.github.com>
2018-10-13 17:09:43 -0400

    Mistakes have been made. Lessons must be learned.

---
+++ commands/mistake.js
@@ -0,0 +1,48 @@
+﻿/*
+    Command Name:
+    Function: 
+    Clearance: none
+	Default Enabled: Yes
+    Date Created: 10/13/18
+    Last Updated: 10/13/18
+    Last Updated By: AllusiveBox
+
+*/
+
+// Load in Required Files
+const Discord = require(`discord.js`);
+const config = require(`../files/config.json`);
+const { run: disabledCommand } = require(`../functions/disabledCommand.js`);
+const { debug, error: errorLog } = require(`../functions/log.js`);
+
+// Command Variables
+const command = {
+    bigDescription: ("This command informs you of what the mistake was."
+        + "Returns\n\t"
+        + config.returnsChannel),
+    description: "Oh, mistakes were made...",
+    enabled: true, // true/false
+    fullName: "Mistake",
+    name: "mistake",
+    permissionLevel: "normal"
+}
+
+/**
+ * 
+ * @param {Discord.Client} bot
+ * @param {Discord.Message} message
+ */
+
+module.exports.run = async (bot, message) => {
+    // Debug to Console
+    debug(`I am inside the ${command.fullName} command.`);
+
+    // Enabled Command Test
+    if (!command.enabled) {
+        return disabledCommand(command.fullName, message);
+    }
+
+    return message.channel.send({ file: "./img/mistake.png" });
+}
+
+module.exports.help = command;
